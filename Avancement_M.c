ba99fe6f096569d6ba7a806a26fddf126313476a
Abewood <101293969+Abewood@users.noreply.github.com>
2022-03-15 11:59:54 +0100

    Add files via upload

--- Avancement_M.c
+++ Avancement_M.c
@@ -25,14 +25,42 @@ void rm_episodes(int nb_ep, struct Mangas *mangas)
     }
 }
 
+void add_chapitres(int nb_chpt, struct Mangas *mangas)
+{
+    int updated_N_chpt = mangas -> chapitres + nb_chpt;
+    mangas -> chapitres = updated_N_chpt;
+}
+
+void rm_chapitres(int nb_chpt, struct Mangas *mangas)
+{
+    int updated_N_chpt = mangas -> chapitres - nb_chpt;
+    if (updated_N_chpt < 0)
+    {
+        printf("ERROR : Opération incorrecte... \n");
+    } 
+    else
+    {
+        mangas -> chapitres = updated_N_chpt;
+    }
+}
+
 int main()
 {
     struct Mangas OP = { "One Piece", 1000, 1043};
+    printf("Avancement sur %s : %i EPs & %i CHs \n", OP.nom, OP.episodes, OP.chapitres);
     add_episodes(1, &OP);
+    printf("----- VERIFICATION EPISODES ----- \n");
     printf("Nombre visionnes : %i \n", OP.episodes);
     rm_episodes(1002, &OP);
     printf("Nombre visionnes : %i \n", OP.episodes);
     //printf("%s : %d, %d \n", OP.nom, OP.episodes, OP.chapitres);
-
+    printf("--------------------------------- \n \n");
+    
+    printf("----- VERIFICATION CHAPITRES ----- \n");
+    add_chapitres(5, &OP);
+    printf("Nombre lus : %i \n", OP.chapitres);
+    rm_chapitres(1050, &OP);
+    printf("Nombre lus : %i \n", OP.chapitres);
+    printf("----------------------------------");
     return 0;
 }
